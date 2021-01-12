fdf8dd12d789034ddbdb979dc7adf1edd9e9a7cb
Hoang Tham <nththam.17it2@vku.udn.vn>
2021-01-12 20:21:01 +0700

    tham#fix

--- src/components/StoryDetail/Content.js
+++ src/components/StoryDetail/Content.js
@@ -6,7 +6,8 @@ import htmlToDraft from 'html-to-draftjs';
 import { Editor } from 'react-draft-wysiwyg';
 
 import { html } from './data';
-import { Box } from '@material-ui/core';
+import {Container ,Grid, Box } from '@material-ui/core';
+import { Edit } from '@material-ui/icons';
 
 class ConvertToRawDraftContent extends Component {
   constructor(props) {
@@ -34,13 +35,20 @@ class ConvertToRawDraftContent extends Component {
     const { editorState } = this.state;
     return (
       <>
-        <Box overflow="hidden" width="100%" className="rdw-storybook-root">
-          <Editor
-            editorState={editorState}
-            toolbarHidden
-            onEditorStateChange={this.onEditorStateChange}
-          />
-        </Box>
+      <Container>
+        <Grid Flex container>
+          <Grid item md={3} justify="center" alignItems="center">Tmidori</Grid>
+          <Grid item md={9} justify="center">
+            <Box overflow="hidden" width="100%" className="rdw-storybook-root">
+              <Editor
+                editorState={editorState}
+                toolbarHidden
+                onEditorStateChange={this.onEditorStateChange}
+              />
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
       </>
     );
   }
